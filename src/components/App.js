f3b3b0989cea06daff77d66394c24c8d87bb83e8
Feba Rachel Lalu <114395573+Fe195@users.noreply.github.com>
2023-01-07 11:35:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom';
 import '../styles/App.css';
 
+const App = () => {
+  //Write your code here
+  const [queryParams] = useSearchParams();
+  const name = queryParams.get('name');
+  const id = queryParams.get('id');
+  const designation = queryParams.get('designation');
 
-function App() {
-  const queryParams = new URLSearchParams(window.location.search)
-  const id = queryParams.get("id")
-  const name = queryParams.get("name")
-  const designation = queryParams.get("designation")
-
-  return (
-    <div className="App">
-      <p>Employee{id}{name}{designation}</p>
-    </div>
-  )
+  return <div className='details'>Employee {id} named {name} works as {designation}</div>
 }
 
 export default App;
